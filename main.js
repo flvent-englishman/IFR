e9663f0705483f78fbd18955606bb7b2478b195e
Really Real <141452587+TheReallyReal@users.noreply.github.com>
2023-08-04 16:05:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,23 @@
+function ifr() {
+ const site = window.location.href;
+ if (site.startsWith("https://google.com")) {
+   var iframe = document.createElement("iframe");
+   const src = prompt("Link:");
+   if (src.startswith("http")) {
+     iframe.src = src;
+     } else {
+       iframe.src = "https://" + src;
+       }
+       iframe.style.width = "100%";
+       iframe.style.height = "100%";
+       iframe.style.position = "fixed";
+       frame.style.top = "0";
+       iframe.style.left = "0";
+       iframe.style.zIndex = "9999";
+       iframe.style.border = "none";
+       document.body.appendChild(iframe);
+ } else {
+   alert("⛔ ERR | Go to 'https://google.com/' to use.");
+ }
+}
 
