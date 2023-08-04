673bea2ad900015b8e4cb5dd8bd9c936c40647b0
Really Real <141452587+TheReallyReal@users.noreply.github.com>
2023-08-04 16:42:48 -0400

    Update bookmarklet.js

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,1 +1,22 @@
-javascript:(function() {const site = window.location.href; if (site.startsWith("https://google.com")) {var iframe = document.createElement('iframe'); const src = prompt("Link:"); if (src.startswith("http")) {iframe.src = src;} else {iframe.src = "https://" + src;} iframe.style.width = '100%'; iframe.style.height = '100%'; iframe.style.position = 'fixed'; frame.style.top = '0'; iframe.style.left = '0'; iframe.style.zIndex = '9999'; iframe.style.border = "none"; document.body.appendChild(iframe);} else {alert("⛔ ERR | Go to 'https://google.com/' to use.");}})();
+javascript:(
+  function() {
+    const site = window.location.href; 
+    var iframe = document.createElement('iframe'); 
+    const src = prompt('Input a link or leave blank to go to Google.'); 
+    if (src === null || src === '') {
+      iframe.src = 'https://google.com/';
+    } else if (src.startswith('http')) {
+      iframe.src = src;
+    } else {
+      iframe.src = 'https://' + src;
+    } 
+    iframe.style.width = '100%'; 
+    iframe.style.height = '100%'; 
+    iframe.style.position = 'fixed';
+    iframe.style.top = '0'; 
+    iframe.style.left = '0'; 
+    iframe.style.zIndex = '9999'; 
+    iframe.style.border = 'none'; 
+    document.body.appendChild(iframe);
+    }
+)();
